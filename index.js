ae43844e90091b8ab210a986efa52709b1109e61
Navaneeth V <navaneeth.v@codilar.com>
2020-08-18 10:27:33 +0530

    WIP inital setup

---
+++ index.js
@@ -0,0 +1,36 @@
+window.onload = () => {
+  let options = {
+    threshold: 0,
+  }
+
+  let rotateLens = (e) => {
+      let lens = document.querySelectorAll('.lens')
+      let parent = document.getElementById('lens-hero-main');
+      parent.style.transform = 'rotate(' + window.pageYOffset / 20 + 'deg)'
+      lens.forEach(len => {
+        len.style.transform = 'rotate(' + window.pageYOffset / 20 + 'deg)'
+      })
+  }
+  let callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      console.log(entry.intersectionRatio);
+      if (entry.isIntersecting) {
+        console.log('visible')
+        entry.target.style.opacity = 1;
+        entry.target.classList.add('sticky')
+        entry.target.classList.add('bottom-0')
+        document.addEventListener('scroll', rotateLens)
+      } else {
+        console.log('hidden')
+        entry.target.classList.remove('sticky')
+        entry.target.classList.remove('bottom-0')
+        document.removeEventListener('scroll', rotateLens)
+      }
+    })
+  }
+
+  let observer = new IntersectionObserver(callback, options)
+
+  let sections = document.getElementById('lenssection')
+  observer.observe(sections)
+}
